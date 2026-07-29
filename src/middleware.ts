d317cf6df6a03116053ad66465226d64d4bc1354
mamonrabin <almamon757@gmail.com>
2026-07-30 03:55:05 +0600

    start project

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+
+
+const middleware = () => {
+    return ;
+};
+
+export default middleware;
---
+++ src/utils/menuList.tsx
@@ -0,0 +1,24 @@
+export const menuList = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "Shop",
+    link: "/shop",
+    subMenu: true,
+  },
+
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Blogs",
+    link: "/blog",
+  },
+  { 
+    title: "Contact us",
+    link: "/contact",
+  },
+];
